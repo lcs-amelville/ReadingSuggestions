5e432e874395b265b70d66460aec65314c5e9b7c
Aidan Melville <amelville@lcs.on.ca>
2021-01-28 10:56:52 -0500

    Initial Commit

---
+++ Shared/ReadingSuggestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingSuggestionsApp.swift
+//  Shared
+//
+//  Created by Melville, Aidan on 2021-01-28.
+//
+
+import SwiftUI
+
+@main
+struct ReadingSuggestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
